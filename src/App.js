26ab8b27bbf14ce3aa5eda206d9167d24faa5cdb
MOR NDIAYE <ndiaye_c@etna-alternance.net>
2019-01-13 09:49:47 +0100

    Initial commit of the ending project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+// Main Component
+const App = () => <PersonList />;
+
+// List Component
+const PersonList = () => {
+  const listOfPerson = [
+    {
+      name:"Morisson", job:"Web Developper", img:"25", sex:"men"
+    }
+    ,
+    {
+      name:"Morissen", job:"Developper", img:"34", sex:"men"
+    }
+    ,
+    {
+      name:"Margaux", job:"Designer", img:"45", sex:"women"
+    }
+  ]
+  
+  return (
+		<section>
+			<Person person={listOfPerson[0]}/>
+			<Person person={listOfPerson[1]}>
+        About more information in my project, please contact me.
+      </Person>
+			<Person person={listOfPerson[2]}>
+        Hey, i am margaux the designer of Villette makerz.  
+      </Person>
+		</section>
+	);
+};
+//Person Component
+const Person = (props) => {
+  const { name, job, img, sex } = props.person;
+  const {children} = props;
+  const url = `https://randomuser.me/api/portraits/thumb/${sex}/${img}.jpg`;
+	return (
+		<div className="person">
+			<img src={url} alt={"person"} />
+			<h4>{name}</h4>
+			<h4>{job}</h4>
+			<p>{children}</p>
+		</div>
+	);
+};  
 
 export default App;
