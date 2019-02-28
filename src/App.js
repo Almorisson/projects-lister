abd2784ee5c21dc74d8994922158f391d2dc7018
Mor NDIAYE <ndiaye_c@etna-alternance.net>
2019-02-28 19:31:57 +0100

    27-02-18 : Modifying some tipos

--- src/App.js
+++ src/App.js
@@ -21,27 +21,32 @@ const PersonList = () => {
   
   return (
 		<section>
-			<Person person={listOfPerson[0]}/>
+      <Person person={listOfPerson[0]}>
+        <p>Hey guys ! I am Morisson and i love React andt all stuff related to JavaScript</p>
+      </Person>
+
 			<Person person={listOfPerson[1]}>
         About more information in my project, please contact me.
       </Person>
+      
 			<Person person={listOfPerson[2]}>
         Hey, i am margaux the designer of Villette makerz.  
       </Person>
 		</section>
 	);
 };
+
 //Person Component
 const Person = (props) => {
   const { name, job, img, sex } = props.person;
-  const {children} = props;
+  const {details} = props;
   const url = `https://randomuser.me/api/portraits/thumb/${sex}/${img}.jpg`;
 	return (
 		<div className="person">
 			<img src={url} alt={"person"} />
 			<h4>{name}</h4>
 			<h4>{job}</h4>
-			<p>{children}</p>
+			<p>{details}</p>
 		</div>
 	);
 };  
